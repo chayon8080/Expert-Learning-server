4f307d1a12fc76c362e1daf222b0e6a8159b85dc
Chayon <chayon15-3633@diu.edu.bd>
2022-10-27 15:29:26 +0600

    third commit

--- index.js
+++ index.js
@@ -3,11 +3,28 @@ const app = express();
 // const cors = require('cors')
 const port = process.env.PORT || 5000;
 // app.use(cors());
-
+const catagories = require('./catagories.json');
+const course = require('./Course.json')
 app.get('/', (req, res) => {
     res.send('Course API Running')
 });
+app.get('/course-catagories', (req, res) => {
+    res.send(catagories)
 
+});
+app.get('/catagory/:id', (req, res) => {
+    const id = req.params.id;
+    const catagorycourse = course.filter(n => n.category_id === id);
+    res.send(catagorycourse)
+});
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedcourse = course.find(n => n._id === id);
+    res.send(selectedcourse)
+});
+app.get('/course', (req, res) => {
+    res.send(course)
+})
 app.listen(port, () => {
     console.log('Expert Learning is runnig', port)
 })
